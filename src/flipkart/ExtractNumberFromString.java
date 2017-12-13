3e32505b5cafcede3ab73058cd3027c6e3fb0a8f
devandara90ks@gmail.com <devandara90ks@gmail.com>
2017-12-14 01:01:00 +0530

    first commit

---
+++ src/flipkart/ExtractNumberFromString.java
@@ -0,0 +1,22 @@
+package flipkart;
+
+import java.util.Scanner;
+
+public class ExtractNumberFromString {
+	public static void main(String[] args) 
+	  {
+	      String str;
+	      String numbers;
+	       
+	      Scanner SC=new Scanner(System.in);
+	       
+	      System.out.print("Enter string that contains numbers: ");
+	      str=SC.nextLine();
+	       
+	      //extracting string
+	      numbers=str.replaceAll("[^0-9]", "");
+	       
+	      System.out.println("Numbers are: " + numbers);
+	  }
+
+}
